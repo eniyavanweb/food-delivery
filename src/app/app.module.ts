a11786c7d02882884b7d813bc69f11dc08aa6615
eniyavan <71455480+eniyavanweb@users.noreply.github.com>
2020-12-17 03:55:44 -0800

    food delivery app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,23 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { Routes, RouterModule } from '@angular/router';
+
+const routes: Routes = [
+  
+  {path:'home',component:HomeComponent}
+  
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
